8b6cfe7763e787ad0397f47a73b34931552196aa
thibaud.besseau@gmail.com <wNFSetlKk!Vc'EGh9W7GKvQ*cefKnhzQ>
2018-10-24 07:32:14 +0200

    login work

--- site/html/config/db.php
+++ site/html/config/db.php
@@ -5,18 +5,18 @@
        }
 
        protected function getUsersPassword($email) {
-        $sql = 'SELECT password  FROM   user  WHERE  email = :email';
+        $sql = 'SELECT Password  FROM   user  WHERE  Email = :email';
         $statement = $this->prepare($sql);
         $statement->bindValue(':email', $email);
         $result = $statement->execute();
         $row = $result->fetchArray();
-        $password = $row['password'];
+        $password = $row['Password'];
         $statement->close();
         return $password;
     }
 
        protected function userExists($email) {
-           $sql = 'SELECT COUNT(*) AS count FROM   user WHERE  email = :email';
+           $sql = 'SELECT COUNT(*) AS count FROM user WHERE  Email = :email';
            $statement = $this->prepare($sql);
            $statement->bindValue(':email', $email);
 
@@ -40,22 +40,34 @@
        }
 
        public function loginUser($email, $password) {
+           $connected=false;
            if ($this->userExists($email)) {
+
                $password_db = $this->getUsersPassword($email);
+
 
                if ($password === $password_db) {
                    $connected = true;
-               } else {
-                   $connected = false;
                }
-           } else {
-               $connected = false;
-           }
 
+
+           }
 
            return $connected;
        }
 
+
+       public function emailUser($email) {
+           $sql = 'SELECT *  FROM message LEFT JOIN user ON message.Recipient = user.Id   WHERE  user.Email = :email';
+           $statement = $this->prepare($sql);
+           $statement->bindValue(':email', $email);
+           $result = $statement->execute();
+           $row = $result->fetchArray();
+           $password = $row['Password'];
+           $statement->close();
+           return $password;
+           return $connected;
+       }
 
 
 
--- site/html/src/login.php
+++ site/html/src/login.php
@@ -1,29 +1,14 @@
 <?php
-require_once("../config/config.php");
+require_once("../config/db.php");
 
     $db = new MyDB();
-    if(!$db) {
-        echo $db->lastErrorMsg();
-    } else {
-        echo "Opened database successfully\n";
-    }
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-       if(isset($_POST['inputEmail']))
-       {
-           echo("set");
-       }
-       else
-       {
-           echo("game");
-       }
-
-       // username and password sent from form
+        // username and password sent from form
       $post_Email = $_POST['inputEmail'];
       $post_Password = $_POST['inputPassword'];
 
 
-        echo($post_Email);
        //test if the user give the correct informations
        if ($db->loginUser($post_Email, $post_Password)===true)
        {
@@ -31,13 +16,12 @@ require_once("../config/config.php");
            $cookie_name = "login";
            $cookie_value = $post_Email;
            setcookie($cookie_name, $cookie_value, time() + (3600 * 30), "/"); // 1 day expiration
-           header("location: index.php");
+           header("location: ../index.php");
 
        }
        else
        {
-           echo($db->loginUser($post_Email, $post_Password));
-           //header("location: ../login.html");
+           header("location: ../login.html");
 
        }
    }
